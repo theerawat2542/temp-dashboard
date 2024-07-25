d9a9cf5d4797c2fc8948b46d114ef3fd07eca0d3
JiScPe <129743223+JiScPe@users.noreply.github.com>
2024-07-25 15:43:19 +0700

    update new backline ui

--- app/(main)/repair/RepairComponent.tsx
+++ app/(main)/repair/RepairComponent.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import RepairStation from "./RepairStation";
+import { RepairI } from "@/app/types/repair-type";
 
 const RepairComponent = () => {
   const searchParams = useSearchParams();
   const plant = searchParams.get("plant");
-  const [dataList, setdataList] = useState([]);
+  const [dataList, setdataList] = useState<RepairI[]>([]);
 
   function fetchRepairData() {
     fetch(`/api/repair_rate?plant=${plant}`)
@@ -24,41 +26,33 @@ const RepairComponent = () => {
     };
   }, [plant]);
 
-  return (
-    <div className="mt-5">
-      <table className="text-white">
-        <thead>
-          <tr>
-            <th>line</th>
-            <th>scan station</th>
-            <th>input</th>
-            <th>output</th>
-            <th>remain</th>
-          </tr>
-        </thead>
-        <tbody>
-          {dataList?.map(
-            ({
-              input_qty,
-              output_qty,
-              remain_qty,
-              line_code,
-              scan_station,
-            }) => (
-              <tr key={line_code + scan_station}>
-                <td>{line_code}</td>
-                <td>{scan_station}</td>
-                <td>{input_qty}</td>
-                <td>{output_qty}</td>
-                <td>{remain_qty}</td>
-              </tr>
-            )
-          )}
-        </tbody>
-      </table>
+  const lineList = dataList
+    .map(({ line_code }) => line_code)
+    .reduce((acc, curr) => {
+      // If the accumulator (acc) does not already include the current value (curr)
+      if (!acc.includes(curr)) {
+        // Add the current value to the accumulator
+        acc.push(curr);
+      }
+      // Return the updated accumulator for the next iteration
+      return acc;
+    }, [] as string[]);
 
-      {/* Layout */}
-      <div></div>
+  return (
+    <div className="mt-5 container mx-auto h-[screen-80px]">
+      {lineList.map((line) => (
+        <div className="m-3" key={line}>
+          <div className="border-2 border-haier-blue p-2 text-center mb-3 text-white">{line}</div>
+          <div className="grid grid-cols-4 gap-2">
+            {dataList
+              .filter(({ line_code }) => line_code === line)
+              .sort((a, b) => a.rank - b.rank)
+              .map((item) => (
+                <RepairStation repairData={item} />
+              ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
